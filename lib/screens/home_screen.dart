af71de6ad5d1000faa0a643774780ec32057ec10
EruelUrsua <eruelursua@gmail.com>
2025-02-04 18:50:40 +0800

    added 1 more card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,6 +23,7 @@ class HomeScreen extends StatelessWidget {
               CelebCard(name: 'Umezawa Minami', imageURL: 'https://pbs.twimg.com/profile_images/1859537749831012352/wmpwuXsO_400x400.jpg', tagline: 'Nogizaka46 Member '),
               CelebCard(name: 'Noguchi Iori', imageURL: 'https://static.wikia.nocookie.net/jpop/images/a/ab/Noguchi_Iori_Zettai_Idol.jpg/revision/latest/smart/width/250/height/250?cb=20240714190151', tagline: '=LOVE Member '),
                CelebCard(name: 'Matsumoto Momona', imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5UfNV5g4Zr7yjoBZqNHGltqP4c9JJ7og9pA&s', tagline: 'Takane no Nadeshiko member '),
+                CelebCard(name: 'Murayama Yuiri', imageURL: 'https://i.namu.wiki/i/2zx7jF8jSgYY1ah8k0f5_FsMXO9_4Df_Hx7PK1GwniaZEbpHDlX0J1WZKCok5GfQ7Ib8wL0195owkU5d3ivUuA.webp', tagline: 'Akb48 member '),
             ],
            ),
          ),
